ddb8cc63b3afc5e5c7853ef183a305eb655a4abb
Madison <madisonjohnson301@gmail.com>
2024-01-09 22:44:15 -0500

    set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import MainPage from './Pages/MainPage/MainPage'
+import MonthPage from './Pages/MonthPage/MonthPage'
+import WeekPage from './Pages/WeekPage/WeekPage'
+import DayPage from './Pages/DayPage/DayPage'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route/>
-          <Route/>
-          <Route/>
-          <Route/>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/:month" element={<MonthPage/>}/>
+          <Route path="/:month/:week" element={<WeekPage/>}/>
+          <Route path="/:month/:week/:day" element={<DayPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
