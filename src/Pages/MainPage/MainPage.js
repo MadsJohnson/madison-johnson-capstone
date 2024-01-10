27ef319d6bf3d7cebb8cc1355791d8651bb99000
Madison <madisonjohnson301@gmail.com>
2024-01-09 23:18:12 -0500

    create and style main page - mobile

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,9 +1,25 @@
 import './MainPage.scss'
+import Chevron from '../../Assets/Icons/chevron_right-24px.svg'
 
 function MainPage() {
 
     return (
-        <div>Main Page</div>
+        <div className='mainpage'>
+            <div className='mainpage__planner'>
+                <div className='mainpage__planner--cover'>
+                    <div className='mainpage__planner--page-turn-icon--container'>
+                        <img src={Chevron} className='mainpage__planner--page-turn-icon'alt='page turn icon'/>
+                    </div>
+                    <div className='mainpage__planner--title--container'>
+                        <div className='mainpage__planner--title--subcontainer'>
+                            <h1 className='mainpage__planner--title'>2024</h1>
+                            <p className='mainpage__planner--subtitle'>PLANNER</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
