108a6f02536e71ea194d4206d2b19d8b1d69fe91
Madison <madisonjohnson301@gmail.com>
2024-01-10 22:11:07 -0500

    add event handler for event input

--- src/Components/DayEventCalendar/DayEventCalendar.js
+++ src/Components/DayEventCalendar/DayEventCalendar.js
@@ -1,13 +1,27 @@
 import './DayEventCalendar.scss'
+import { useState } from 'react';
 
 function DayEventCalendar() {
+  const [events, setEvents] = useState(''); 
+
+  const handleEventInput = (event) => {
+    const newEvent = event.target.value;
+    setEvents(newEvent)
+
+    console.log(events)
+
+    //next step: POST to API to save event
+  }
+
 
   return (
     <div className="calendar-day">
       <div className="calendar-day__number">DAY #</div>
       <textarea
         className="calendar-day__user-input"
-        placeholder=""
+        placeholder="enter event here..."
+        value={events}
+        onChange={handleEventInput}
       />
     </div>
   );
