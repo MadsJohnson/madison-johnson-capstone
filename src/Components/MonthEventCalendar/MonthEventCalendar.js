39a88e0f4ba7e2459a07c8ed1e83320ed3b06d16
Madison <madisonjohnson301@gmail.com>
2024-01-10 22:47:19 -0500

    function to get the number of day in all months of the year

--- src/Components/MonthEventCalendar/MonthEventCalendar.js
+++ src/Components/MonthEventCalendar/MonthEventCalendar.js
@@ -1,12 +1,38 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import DayEventCalendar from '../DayEventCalendar/DayEventCalendar';
 import './MonthEventCalendar.scss'
 
 function MonthEventCalendar() {
 
     const { month } = useParams();
-
     const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const [currentDate, setCurrentDate] = useState(new Date()); // Current month and year
+    const [daysOfMonth, setDaysOfMonth] = useState([]);
+
+
+    useEffect(() => {
+        // Function to get days in a specific month
+        function daysInMonth(year, month) {
+          const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+          const daysArray = [];
+    
+          for (let day = 1; day <= lastDayOfMonth; day++) {
+            daysArray.push(day);
+          }
+    
+          return daysArray;
+        }
+    
+        const currentYear = currentDate.getFullYear();
+        const monthIndex = new Date(Date.parse(month + ' 1, ' + currentYear)).getMonth();
+        const daysArray = daysInMonth(currentYear, monthIndex);
+        setDaysOfMonth(daysArray);
+
+        console.log(daysArray)
+    
+      }, [currentDate, month]);
+
 
 
     return (
