1a5aea0135dbc99ccd0378be49027aa3ee95dfde
Madison <madisonjohnson301@gmail.com>
2024-01-13 18:47:46 -0500

    fix file name of week navigation component

--- src/Pages/MonthPage/MonthPage.js
+++ src/Pages/MonthPage/MonthPage.js
@@ -1,11 +1,20 @@
-import MonthEventCalendar from '../../Components/MonthEventCalendar/MonthEventCalendar';
+import MonthEventCalendar from '../../Components/MonthEventCalendar/MonthEventCalendar'
+import WeekOfMonthNav from '../../Components/WeekOfMonthNav/WeekOfMonthNav';
 import './MonthPage.scss'
 
 function MonthPage() {
 
     return (
         <div className='month-page'>
-            <MonthEventCalendar />
+            <div className='month-page__planner'>
+                <div className='month-page__planner--page'>
+                <MonthEventCalendar />
+                
+                </div>
+                <WeekOfMonthNav/>
+                
+            </div>
+          
         </div>
 
     )
