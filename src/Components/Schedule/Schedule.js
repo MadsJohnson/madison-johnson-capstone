c2e26836ed0aa789b72c6009e46a66da779260e5
Madison <madisonjohnson301@gmail.com>
2024-01-28 12:40:19 -0500

    update use effect
    - new items were only being posted on days that agenda data existed already. Problem solved.
    - updated useeffect for cases where there is not agendadata in database
    - added logic to the input value to post empty string if no value is assigned

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -30,18 +30,21 @@ const Schedule = ({ agendaData, date, baseUrl }) => {
     const timerRef = useRef(null);
 
     useEffect(() => {
-        if (agendaData) {
-            const updatedSchedule = { ...initialSchedule };
-    
-            agendaData.forEach(item => {
-                const { time, task } = item;
-                // Use the time directly as the key in the schedule
-                updatedSchedule[time] = task;
-            });
-    
-            setSchedule(updatedSchedule);
+        if (agendaData && agendaData.length > 0) {
+          const updatedSchedule = { ...initialSchedule };
+      
+          agendaData.forEach(item => {
+            const { time, task } = item;
+            // Use the time directly as the key in the schedule
+            updatedSchedule[time] = task;
+          });
+      
+          setSchedule(updatedSchedule);
+        } else {
+          // Map over the initial schedule if agendaData is empty
+          setSchedule(initialSchedule);
         }
-    }, [agendaData]);
+      }, [agendaData]);
 
     const postAgendaItem = (date, time, task) => {
         const token = sessionStorage.token;
@@ -85,23 +88,22 @@ const Schedule = ({ agendaData, date, baseUrl }) => {
     const handleAddItem = (newTime, newValue) => {
         // Check if agendaData is available and has a length greater than 0
         if (agendaData && agendaData.length > 0) {
-            // Check if an entry with the same date and time already exists
-            const existingEntry = agendaData.find(item => item.date === date && item.time === newTime);
-    
-            if (!existingEntry) {
-                // Post the agenda item if it doesn't exist
-                postAgendaItem(date, newTime, newValue);
-            } else {
-                // Update the existing agenda item if it exists
-                updateAgendaItem(existingEntry.id, date, newTime, newValue);
-            }
+          // Check if an entry with the same date and time already exists
+          const existingEntry = agendaData.find(item => item.date === date && item.time === newTime);
+      
+          if (!existingEntry) {
+            // Post the agenda item if it doesn't exist
+            postAgendaItem(date, newTime, newValue);
+          } else {
+            // Update the existing agenda item if it exists
+            updateAgendaItem(existingEntry.id, date, newTime, newValue);
+          }
         } else {
-            // If agendaData is null or empty, wait for a few seconds and try again
-            setTimeout(() => {
-                handleAddItem(newTime, newValue);
-            }, 2000); 
+          // If agendaData is null or empty, post the agenda item
+          postAgendaItem(date, newTime, newValue);
         }
-    };
+      };
+      
 
     const updateAgendaItem = (id, date, time, task) => {
         const token = sessionStorage.token;
@@ -129,26 +131,27 @@ const Schedule = ({ agendaData, date, baseUrl }) => {
 
     return (
         <div className='schedule'>
-            <h2 className='schedule__title'>Schedule</h2>
-            <table className='schedule__container'>
-                <tbody className='schedule__subcontainer'>
-                    {Object.entries(schedule).map(([time, task]) => (
-                        <tr className='schedule__row' key={time}>
-                            <td className='schedule__row--time'>{time}</td>
-                            <td className='schedule__row--input--container'>
-                                <input
-                                    className='schedule__row--input'
-                                    type="text"
-                                    value={task}
-                                    onChange={e => handleInputChange(time, e.target.value)}
-                                />
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+          <h2 className='schedule__title'>Schedule</h2>
+          <table className='schedule__container'>
+            <tbody className='schedule__subcontainer'>
+              {Object.entries(initialSchedule).map(([time, task]) => (
+                <tr className='schedule__row' key={time}>
+                  <td className='schedule__row--time'>{time}</td>
+                  <td className='schedule__row--input--container'>
+                    <input
+                      className='schedule__row--input'
+                      type="text"
+                      value={schedule[time] || ''}
+                      onChange={e => handleInputChange(time, e.target.value)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
-};
+      );
+      
+    ;}
 
-export default Schedule;
+      export default Schedule;
