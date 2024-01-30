cbe79536cf625ece7eeaa9194406ee472669fe5a
Madison <madisonjohnson301@gmail.com>
2024-01-29 20:29:44 -0500

    create handle input function to update state variable on input

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './Notes.scss';
 
-const Notes = () => {
+const Notes = (notesData, date) => {
+
+const [noteText, setNoteText] = useState('');
+
+const handleInputChange = (event) => {
+    setNoteText(event.target.value);
+};
+
+console.log(noteText)
+
     return (
         <div className="notes">
             <h2 className="notes__title">Notes</h2>
             <textarea
-
                 className="notes__text-input"
                 type="text"
-            
+                value={noteText}
+                onChange={handleInputChange}
             ></textarea>
 
         </div>
