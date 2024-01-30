1f7557d94d1eb22e18ec12ba1b309158e986c079
Madison <madisonjohnson301@gmail.com>
2024-01-29 21:05:16 -0500

    add post request to back end of empty component and assign the noteId variable to the posted notes Id
    update use effect to set the noteId and text state if notesData is available

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,19 +1,55 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Notes.scss';
 
-const Notes = ({notesData, date}) => {
+const Notes = ({notesData, date, baseUrl}) => {
 
     const [noteText, setNoteText] = useState('');
+    const [noteId, setNoteId] = useState(null)
 
 
     useEffect(() => {
         if (notesData && notesData.length > 0) {
             setNoteText(notesData[0].note);
+            setNoteId(notesData[0].note_id)
+        }
+
+        else {
+            if (notesData && notesData.length === 0) {
+                postNote();
+            }
         }
 
     }, [notesData]);
 
     console.log(noteText)
+    console.log(noteId)
+
+    const postNote = () => {
+        const token = sessionStorage.token;
+        axios.post(
+            `${baseUrl}/notes`,
+            {
+                note: noteText,
+                due_date: date,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        )
+            .then(response => {
+                console.log('note posted successfully:', response.data);
+                // Set the note_id in the state
+                setNoteId(response.data.note_id);
+            })
+            .catch(error => {
+                console.error('Error posting note :', error);
+            });
+    };
+
+
 
     const handleInputChange = (event) => {
         setNoteText(event.target.value);
